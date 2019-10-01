470cd4057f1314cb57260adb123fe264d9b58110
Patricia <prw250@nyu.edu>
2019-10-01 14:26:13 +0800

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>The Stack: Netflix Binge Potential </h1>
+        <p>This stack is analyzing the issue Netflix's vast amount of content and how it can be very time consuming and distracting.  </p>
+        <h3>USER</h3>
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToMBVknaX0JMLYA0fDvPTXCtAj4qBIgMC---bUQsTjRQfJjeAj" alt="Me" width="500"/>
+       <p>The user (me) distracted by Netflix and unproductive.</p>
+       <h3>INTERFACE</h3>
+       <img src="https://miro.medium.com/max/5756/1*NFLGOjswzul9L5M9vpGnYg.jpeg" alt="Netflix interface" width="500"/>
+       <p>The Netflix interface with it's hours upon hours of consumable content.</p>
+       <h3>ADDRESS</h3>
+       <img src="https://www.cisco.com/c/en/us/products/security/anyconnect-secure-mobility-client/index/_jcr_content/Grid/category_atl/layout-category-atl/anchor_info_c39c/image.img.jpg/1556802660151.jpg" alt="vpn" width="500"/>
+       <p>The NYU Hong Kong VPN that allows me to access Hong Kong Netflix from Shanghai.</p>
+       <h3>CITY</h3>
+       <img src="https://lh5.googleusercontent.com/WAIetSIPeLUzL4pWWDxTYQ7EY-7cImjk5TKhNOCmbR52_2uMII9NhjGRuwBsLg1k6x2TUBLeLnnXqQ09VkSKkyLg8iqi9rQZ_TyyE3yT6v4gVM71YW2N7vmuy3IfXZJfv-YR2eCl" alt="data centers" width="500"/>
+       <p>Netflix uses third party data centers and there is one in Hong Kong storing their data.</p>
+       <h3>CLOUD</h3>
+       <img src="https://zdnet1.cbsistatic.com/hub/i/r/2014/10/05/e0f1f796-4c69-11e4-b6a0-d4ae52e95e57/resize/770xauto/7ad496808634bd708018b459edf59cc8/netflixcloud-620x457.png" alt="data centers" width="500"/>
+       <p>Netflix controls their data through the cloud and are considered by some as the biggest cloud app.</p>
+       <h3>EARTH</h3>
+       <img src="https://metro.co.uk/wp-content/uploads/2019/03/SEC_56319250.jpg?quality=90&strip=all" alt="data centers" width="500"/>
+       <p>Netflix has users all over the globe, even in places it's not technically allowed.</p>
     </div>
   );
 }
